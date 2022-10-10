2cd54a3cb1ae19a3c4874bcfa7d48c8f20da29b7
MAIL404 <113687471+MAIL404@users.noreply.github.com>
2022-10-11 02:08:59 +0700

    Update esce.js

--- plugins/esce.js
+++ plugins/esce.js
@@ -2,13 +2,7 @@
 // penjelasan berfungsi buat user lain bila gak tau
 
 let handler = async (m, { conn, command }) => {
-    conn.reply(m.chat, `*BETA V2.0.1.6*
-• *Multi Auth ( Multiple File Session )*
-• https://github.com/BOTCAHX/RT-X-MD
-• Script ini mempunyai multiple session 
-• *Jika ingin melihat apa itu multiple session lihat link di bawah ini.*
-• *Preview* : https://telegra.ph/file/fd3362fea5132cc59fed2.jpg 
-• *Preview* : https://telegra.ph/file/fa2a66e8cca50f62885ab.jpg
+    conn.reply(m.chat, `X
 `, m)
 }
 
