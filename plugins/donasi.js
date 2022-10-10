e10a16f03e36e7552ef9598830d6919fba68c28a
MAIL404 <113687471+MAIL404@users.noreply.github.com>
2022-10-11 02:14:07 +0700

    Hh

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,5 @@
 let handler =  m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Pulsa [082221792667]
-│ • Dana [082221792667]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • Dana [082221792667]
-│ • Pulsa [082221792667]
-╰────
+Pulsa Ceban indosat 08146382902
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
